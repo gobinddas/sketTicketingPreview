777589847978abd968dde40dbcd3b34f776e45cb
SrijeshBista <srijeshbista367@gmail.com>
2025-05-30 16:42:59 +0545

    srijeshbista

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
-import logo from '../Assets/logo.png';
+import logo from '../Assets/SkaterHood.png';
 import './Sidebar.css'; 
+import { FaBars } from 'react-icons/fa';
+import { RxCross1 } from "react-icons/rx";
 
 const Sidebar = () => {
   const navigate = useNavigate();
+  const [openham, setopenham] =useState(false);
+
+  // function to open hamburger
+  const handleopenhamburger =()=>{
+    setopenham(true);
+  }
+  const handleclosehamburger =()=>{
+    setopenham(false);
+  }
+
+
 
   const handleLogout = () => {
     console.log('Logout button clicked');
@@ -15,6 +29,7 @@ const Sidebar = () => {
   };
 
   return (
+    <>
     <div className="sidebar">
       <div className="logo box-white">
         <img src={logo} alt="Logo" />
@@ -64,10 +79,74 @@ const Sidebar = () => {
           </li>
         </ul>
       </nav>
-      <div className="logout">
-        <button onClick={handleLogout}>Logout</button>
-      </div>
+      
+     
+
+     
     </div>
+     {/* hamburger  */}
+     <div className="hambars">
+        <button onClick={handleopenhamburger}><FaBars/></button>
+        {openham && (
+          <>
+            <div className='ham-nav'>
+            <div className="logo box-white">
+        <img src={logo} alt="Logo" />
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              end
+              className={({ isActive }) => (isActive ? 'active-link' : undefined)}
+            >
+              Dashboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/active-session"
+              className={({ isActive }) => (isActive ? 'active-link' : undefined)}
+            >
+              Active Session
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/tickets-price"
+              className={({ isActive }) => (isActive ? 'active-link' : undefined)}
+            >
+              Tickets Price
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/balance-sheet"
+              className={({ isActive }) => (isActive ? 'active-link' : undefined)}
+            >
+              Balance Sheet
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="https://bijuwarsp.bluebugsoft.com/support.php"
+              className={({ isActive }) => (isActive ? 'active-link' : undefined)} target='blank'
+            >
+              Support
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="logout">
+        <button onClick={handleclosehamburger}><RxCross1/></button>
+      </div>
+            </div>
+          </>
+        )}
+      </div>
+        
+    </>
   );
 };
 
